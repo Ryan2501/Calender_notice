6518cb2e6010b15d445a979e5169cb980885fbc1
Ryan2501 <154885180+Ryan2501@users.noreply.github.com>
2024-03-27 15:53:28 +0800

    Update main.dart

--- calender/lib/main.dart
+++ calender/lib/main.dart
@@ -1,9 +1,15 @@
-import 'package:line_notify/line_notify.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
-  final lineNotify = LineNotify(token: 'jNrJSb2n8CyQnrX4YGFJI3AuBeNutGxSXTO9dOyLMwS');
-  final response = await lineNotify.send(message: '這是測試實驗');
-
+  String url = 'https://notify-api.line.me/api/notify';
+  String token = 'jNrJSb2n8CyQnrX4YGFJI3AuBeNutGxSXTO9dOyLMwS';
+  Map<String, String> headers = {
+    'Authorization': 'Bearer $token',    // 設定權杖
+  };
+  Map<String, String> data = {
+    'message': '測試一下！',     // 設定要發送的訊息
+  };
+  http.Response response = await http.post(Uri.parse(url), headers: headers, body: data);   // 使用 POST 方法
   print(response.statusCode);
   print(response.body);
 }
